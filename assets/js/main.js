b1caa142cce6530a8befb044ba70d98287d3fee3
Exord <wecatorz@gmail.com>
2021-02-05 11:01:31 +0300

    Реализация игры с другом на одном пк

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*var area = document.getElementById('area');
+var square = document.getElementsByClassName('square');
+/*
+for (var i = 1; i <= 9; i++){
+    if (i == 1) {
+        area.innerHTML += "<section>";
+        area.innerHTML += "<span class='square' pos=" + i + "></div>";
+    }
+    area.innerHTML += "<span class='square' pos=" + i + "></div>";
+    
+}*/
