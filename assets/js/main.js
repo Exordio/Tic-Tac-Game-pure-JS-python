885ae59731347e51c3b4a15fd806b02e49500efb
Exord <wecatorz@gmail.com>
2021-02-07 21:11:36 +0300

    21:11 update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@ const selectForm = document.querySelector(".select"),
     selectOnepc = selectForm.querySelector(".onepc"),
     selectWithbot = selectForm.querySelector(".withbot"),
 
+    past_games = document.querySelector(".past_games"),
+    past_games_content = past_games.querySelector(".content"),
+
     diffForm = document.querySelector(".form-diff-bot"),
 
     low_diff = document.querySelector(".low"),
@@ -23,12 +26,15 @@ const selectForm = document.querySelector(".select"),
     mainMenu = document.getElementById('mainMenu');
 
 
+
+
 cells = document.getElementsByClassName('cell');
 
 player = "x";
 bot_in_game = false;
 in_multiplauer = false;
 timeDelay = 300;
+log = []
 
 
 const winIndex = [
@@ -44,13 +50,53 @@ const winIndex = [
 
 
 
+function isDict(v) {
+    return !!v && typeof v === 'object' && v !== null && !(v instanceof Array) && !(v instanceof Date) && isJsonable(v);
+}
+
+
 ws = new WebSocket("ws://localhost:8765")
 ws.onmessage = ({
     data
 }) => {
-    turn = false
-    game_log_x = []
-    game_log_o = []
+    turn = false;
+    //TODO LOG MULTIPLAYER
+    game_log_x = [];
+    game_log_o = [];
+
+    try {
+        if (JSON.parse(data)[0].slice(JSON.parse(data)[0].length - 1, JSON.parse(data)[0].length) == "log") {
+            //    console.log(data);
+            //    data_len = data.length
+            //    if(JSON.parse(data)[0].slice(data -1, data.length)
+            var past_games_parse = JSON.parse(data).reverse();
+            //    console.log(past_games_parse);
+
+            // Вывод последних 5 игр.
+            for (let i = 0; i < 5; i++) {
+                try {
+                    let x = past_games_parse[i].slice(past_games_parse[i].length - 3, past_games_parse[i].length)
+
+                    console.log(x);
+                    winner_f_log = x[0]['winner'];
+                    time_f_log = x[1]['time'].slice(0, 21);
+
+                    if (winner_f_log != "draw") {
+                        past_games_content.innerHTML += `<div class="win_block">${time_f_log} Победил : <p> ${winner_f_log.toUpperCase()}</p><div>`
+                    } else {
+                        
+                    }
+                    
+                } catch {
+                    //                console.log('')
+                }
+            }
+
+        }
+    } catch {
+        //console.log("Type error - history was empty?")
+    }
+
     if (in_multiplauer == true) {
         console.log(parseInt(data))
         boxSpans[parseInt(data) - 1].innerHTML = `<p>${player}</p>`;
@@ -110,12 +156,14 @@ ws.onmessage = ({
 window.onload = () => {
     selectOnepc.onclick = () => {
         selectForm.classList.add("hide");
+        past_games.classList.add("hide");
         board_field.classList.add("show");
         bot_in_game = false;
     }
 
     selectWithbot.onclick = () => { //выбор игры с ботом
         selectForm.classList.add("hide");
+        past_games.classList.add("hide");
         diffForm.classList.add("show");
 
     }
@@ -137,6 +185,7 @@ window.onload = () => {
 
     selectMultiplayer.onclick = () => {
         selectForm.classList.add("hide");
+        past_games.classList.add("hide");
         multiplayerSettings.classList.add("show");
         in_multiplauer = true;
     }
@@ -149,15 +198,22 @@ window.onload = () => {
         resultForm.classList.remove("show");
         restart();
         board_field.classList.add("show");
+        log = [];
+        swith_log = false;
     }
 
     mainMenu.onclick = () => {
         resultForm.classList.remove("show");
+        past_games.classList.remove("hide");
         restart();
         selectForm.classList.remove("hide");
-        in_multiplauer = false;
-        bot_in_game = false;
-        minimax_check = false;
+        //        in_multiplauer = false;
+        //        bot_in_game = false;
+        //        minimax_check = false;
+        //        log = [];
+        //        swith_log = false;
+        location.reload();
+
     }
 
 }
@@ -183,11 +239,17 @@ for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', cellClick, false);
 }
 
+swith_log = false;
+
 function cellClick() {
     var data = [];
 
+
     if (!this.innerHTML) {
         this.innerHTML = `<p>${player}</p>`;
+        if (in_multiplauer == false) { //&& (bot_in_game == false)) {
+            logging(parseInt(this.getAttribute('pos')))
+        }
 
         if (in_multiplauer) {
             ws.send(JSON.stringify({
@@ -195,7 +257,6 @@ function cellClick() {
                 'turn': parseInt(this.getAttribute('pos'))
                 //            'my_socket_id': my_socket
             }));
-
         }
 
     } else {
@@ -215,6 +276,15 @@ function cellClick() {
             board_field.setAttribute("class", "board");
             resultForm.setAttribute("class", "result-form show");
             winText.innerHTML = `Игрок <p>${player.toUpperCase()}</p> выйграл в игре`;
+            log.push({
+                //'log': 'True',
+                winner: player
+
+            }, {
+                time: Date(Date.now()).toString()
+            }, "log");
+            console.log(log);
+            pushlog(log); // Отправка лога на websocket server
             player = "x";
             return;
 
@@ -229,6 +299,10 @@ function cellClick() {
                 board_field.setAttribute("class", "board");
                 resultForm.setAttribute("class", "result-form show");
                 winText.innerHTML = `Ничья!`;
+                log.push({
+                    winner: "Draw"
+                })
+                console.log(log);
                 player = "x";
                 return;
             }
@@ -245,8 +319,37 @@ function cellClick() {
 
     if (bot_in_game) {
         setTimeout(() => {
-            bot(minimax_check); //Включение бота, и минимакса
+            bot(minimax_check); //Включение бота, и минимакса если выбрана высокая сложность
         }, timeDelay)
+    }
+
+}
+
+function logging(turn_index) {
+    if (!(swith_log)) {
+        log.push({
+            player_x: parseInt(turn_index)
+        });
+        //        console.log(log);
+        swith_log = true;
+    } else {
+        log.push({
+            player_o: parseInt(turn_index)
+        });
+        //        console.log(log);
+        swith_log = false;
+    }
+}
+
+function bot_logging(turn_index) {
+    if ((in_multiplauer == false) && (bot_in_game == true)) {
+        if (swith_log) {
+            log.push({
+                player_o: parseInt(turn_index + 1)
+            });
+            //            console.log(log);
+            swith_log = false;
+        }
     }
 }
 
@@ -259,16 +362,18 @@ function bot(hard_bot) {
 
         var originalBoard = get_the_board()
         var choise = minimax(originalBoard, "o")
-        console.log(choise)
+        //        console.log(choise)
 
         boxSpans[choise['index']].innerHTML = `<p>${player}</p>`;
 
         for (var i in cells) {
             if (cells[i].innerHTML == `<p>${player}</p>`) {
                 data_bot.push(parseInt(cells[i].getAttribute('pos')));
-                console.log(data_bot);
+                //                console.log(data_bot);
             }
         }
+
+        bot_logging(choise['index']);
 
 
         player = "x";
@@ -284,11 +389,14 @@ function bot(hard_bot) {
                 resultForm.setAttribute("class", "result-form show");
                 winText.innerHTML = `Вы проиграли боту<br>ну и ладно.`;
                 player = "x";
+                log.push({
+                    winner: "bot"
+                })
+
+                console.log(log)
                 return;
             }
         }, timeDelay + 100)
-
-
 
     } else {
 
@@ -298,7 +406,9 @@ function bot(hard_bot) {
             }
         }
         let rand = arr[Math.floor(Math.random() * arr.length)];
-        console.log(rand);
+        //        console.log(rand);
+
+        bot_logging(rand);
 
         if (arr.length > 0) {
             boxSpans[rand].innerHTML = `<p>${player}</p>`;
@@ -307,7 +417,7 @@ function bot(hard_bot) {
         for (var i in cells) {
             if (cells[i].innerHTML == `<p>${player}</p>`) {
                 data_bot.push(parseInt(cells[i].getAttribute('pos')));
-                console.log(data_bot);
+                //                console.log(data_bot);
             }
         }
 
@@ -323,14 +433,17 @@ function bot(hard_bot) {
                 board_field.setAttribute("class", "board");
                 resultForm.setAttribute("class", "result-form show");
                 winText.innerHTML = `Вы проиграли боту<br>ну и ладно.`;
+                log.push({
+                    winner: "bot"
+                })
+                console.log(log);
+
                 player = "x";
                 return;
             }
         }, timeDelay + 100)
 
     }
-
-
 }
 
 function get_the_board() {
@@ -343,7 +456,7 @@ function get_the_board() {
         }
     }
 
-    console.log(board);
+    //    console.log(board);
     return board;
 }
 
@@ -369,10 +482,11 @@ function winning_for_minimax(board, player_minx) {
     }
 }
 
-huPlayer = "x";
-aiPlayer = "o";
 
+// Рекурсивная реализация стратегии
 function minimax(newBoard, player_minx) {
+    var huPlayer = "x";
+    var aiPlayer = "o";
 
     var availSpots = emptyIndex(newBoard);
 
@@ -389,7 +503,6 @@ function minimax(newBoard, player_minx) {
             score: 0
         };
     }
-
 
     var moves = [];
 
@@ -456,4 +569,9 @@ function restart() {
         cells[i].innerHTML = '';
         players.setAttribute("class", "players");
     }
+}
+
+
+function pushlog(log) {
+    ws.send(JSON.stringify(log))
 }
--- websockets/websocket_server.py
+++ websockets/websocket_server.py
@@ -1,6 +1,8 @@
 import asyncio
 import websockets
 import json
+import os
+from datetime import datetime
 
 USERS = set()
 
@@ -10,10 +12,23 @@ async def notify_users():
         message = f'{USERS}'
         await asyncio.wait([user.send(message) for user in USERS])
 
-#TODO канальное распределение.
+
+turns_in_room = []
+history = []
+
+
+# TODO канальное распределение.
 async def register(websocket):
+    global history
     USERS.add(websocket)
     await notify_users()
+    if os.stat("data.json").st_size != 0:
+        with open('data.json') as json_file:
+            history = json.load(json_file)
+        for i in history:
+            print(i)
+
+    await asyncio.wait([user.send(json.dumps(history)) for user in USERS])
 
 
 async def unregister(websocket):
@@ -21,24 +36,34 @@ async def unregister(websocket):
     await notify_users()
 
 
-turns_in_room = []
 # Примитивный метод, репульс пакета от клиента другим клиентам.
 # TODO По хорошему нужно написать алгоритм марсшрутизиции между комнатами в игре.
+
 async def repulse(websocket, path):
+    # asyncio.get_event_loop().create_task(history(websocket))
     try:
         await register(websocket)
         while True:
             data = await websocket.recv()
             data = json.loads(data)
-            print(data)
-            if('turn' in data):
+            if ('turn' in data):
+                print(data)
                 turns_in_room.append(data['turn'])
                 print(turns_in_room)
             if len(turns_in_room) > 0:
                 await asyncio.wait([user.send(json.dumps(turns_in_room[::-1][0])) for user in USERS])
-            if('clear' in data):
+            if ('clear' in data):
                 print('Clearing turns')
                 turns_in_room.clear()
+
+            if ('log' in data):
+                print(data)
+                # print(history)
+                history.append(data)
+                to_file = history
+                with open('data.json', 'w', encoding='utf-8') as f:
+                    json.dump(to_file, f, ensure_ascii=True, indent=4)
+
     except websockets.exceptions.ConnectionClosedOK:
         print('Clearing turns')
         turns_in_room.clear()
